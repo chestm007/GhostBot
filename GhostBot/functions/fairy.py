c405393cbcca299a64f211d22fe1e7d4a2301e5f
chestm007 <chestm007@hotmail.com>
2025-10-28 18:22:34 +1100

    remove redundant code

--- GhostBot/functions/fairy.py
+++ GhostBot/functions/fairy.py
@@ -47,23 +47,3 @@ class Fairy(Locational):
         """
         self._team_members = {i: self._bot_controller.clients.get(name) for i, name in enumerate(self._client.team_members)}
         return self._team_members is not None
-
-    #def __detect_team_members(self):
-    #    """
-    #    Old func, shouldn't be needed
-    #    :return:
-    #    """
-    #    self._client.target_self()
-    #    time.sleep(0.2)
-    #    if self._client.target_name != self._client.name:
-    #        logger.debug(f'{self._client.name}: self not found??')
-    #        return False
-    #    for member_number in self._client.config.fairy.get('team'):
-
-    #        # TODO: we know the memory addresses now, lets just try and use them, fallback to
-    #        #       manual detection if we have too
-    #        self._client.left_click(TeamLocations[member_number])
-    #        time.sleep(0.2)
-    #        self._team_members[member_number] = self.clients.get(self._client.target_name)
-    #    logger.debug(f'{self._client.name}: Team: {" | ".join([m.name for m in self._team_members.values()])}')
-    #    return True
