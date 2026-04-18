839ccc1e6d7ae8f696f11eb334ade06fcd5b082e
chestm007 <chestm007@hotmail.com>
2026-04-18 23:30:55 +1000

    Feature: Add Sky Village and Green Scarp to map_navigation.py

--- GhostBot/map_navigation.py
+++ GhostBot/map_navigation.py
@@ -6,6 +6,8 @@ zones = (
     {
         'simen_mountain': Zone('simen_mountain', ((1270, 1800), (2030, 1280)), (1667, 1544), (-1, 1.2)),
         'barbarian_mountain': Zone('barbarian_mountain', ((1281, 1280), (2050, 510)), (1667, 902), (-1, 1.2)),
+        'green_scarp': Zone('green_scarp', ((280, 1500), (500, 760)), (456, 1164), (-1.05, 1.65)),
+        'sky_village': Zone('sky_village', ((250, 760), (515, 0)), (150, 404), (-1.5, 1.85)),
         'cloud_mountain': Zone('cloud_mountain', ((-200, 220), (760, -266)), (257, 45), (-1.4, 1.5)),  #(274, -116)),
         'stone_city': Zone('stone_city', ((-70, -266), (490, -760)), (217, -528), (-1, 1.2)), # (178, -517)),
         'black_wind_camp': Zone('black_wind_camp', ((-762, -270), (-60, -760)), (-415, -497), (-1, 1.1)), # (-255, -484)),
@@ -28,6 +30,32 @@ _zones_map = {
         'South of Simen Mountain',
     ),
     'west_simen_mountain': ('Dry Woods', 'Old Site of Village', 'West of Simen Mountain'),
+    'green_scarp': (
+        'Green Scarp',
+        'Piedmont of Green Scarp',
+        'Green Scarp Secret Cave',
+        'Back Mountain of Green Scarp',
+    ),
+    'sky_village': (
+        'Sky Village',
+        'Sky Village Field',
+        'Hiking Road',
+        'South Wasteland',
+        'Sky Village North East',
+        'Sky Village South East',
+        'Sky Village South West',
+        'South Suburb of Sky Village',
+        'North Suburb of Sky Village',
+        'East Suburb of Sky Village',
+        'West Suburb of Sky Village',
+        'Snake Swamp',
+        'Snake Swamp South',
+        'Snake Swamp North',
+        'Black Campo',
+        'Black Campo West',
+        'Black Heart Wasteland'
+
+    ),
     'cloud_mountain': (
         'Serene Village',
         'Serene Hiking',
@@ -128,6 +156,7 @@ location_to_zone_map = {sv: k for k, v in _zones_map.items() for sv in v}
 
 def _goto_centre_of_map():
     import logging
+    import time
     from GhostBot import logger
     from GhostBot.lib.win32.process import PymemProcess
     from GhostBot.controller.bot_controller import BotClientWindow
@@ -136,9 +165,15 @@ def _goto_centre_of_map():
     for proc in PymemProcess.list_clients():
         client = BotClientWindow(proc)
         if client.name == "FkYouUp1":
-            #client.press_key('m')
-            wx, wy = client.get_window_size()
-            client.right_click((int(wx/2), int(wy/2)))
+
+            # uncomment to find centre
+            # with client.map():
+            #     wx, wy = client.get_window_size()
+            #     client.right_click((int(wx/2), int(wy/2)))
+
+            # uncomment to test offset calibrations
+            time.sleep(2)
+            client._move_to_pos_via_map((150, 604))
 
 if __name__ == '__main__':
     _goto_centre_of_map()
